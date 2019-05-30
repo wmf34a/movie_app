573724a89a599d3bd5d00b5ecb2b04b383cc0347
Ys-Choi <yseung777@mailplug.co.kr>
2019-05-30 16:30:27 +0900

    Updates

---
+++ precache-manifest.f9ad6f11c9e765278b7744430f564ba0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b681955dee9b1fd5dbcabb88dabda5c9",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "a25ed112910b7f2cc32d",
+    "url": "/movie_app/static/css/main.760f021b.chunk.css"
+  },
+  {
+    "revision": "ad63ab08a2a8e5504836",
+    "url": "/movie_app/static/js/2.7b834e7d.chunk.js"
+  },
+  {
+    "revision": "a25ed112910b7f2cc32d",
+    "url": "/movie_app/static/js/main.07b4e117.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
